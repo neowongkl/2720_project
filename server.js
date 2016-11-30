30886c51eaa29131210d4a79d3dda37bca575f4d
neowongkl <neo.wongkl@gmail.com>
2016-11-30 18:55:17 +0800

    template

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('public/index');
+});
+
+
+app.listen(8080);
+console.log('8080 is the magic port');
